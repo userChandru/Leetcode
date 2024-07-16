c94894ee2d9971f1295dbd0a894f6273a9b68f9d
Chandru <134100579+userChandru@users.noreply.github.com>
2024-07-16 12:08:28 +0530

    Time: 8 ms (88.52%), Space: 11.3 MB (9.21%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.c
+++ 1580-shuffle-the-array/1580-shuffle-the-array.c
@@ -6,9 +6,9 @@
 int* shuffle(int* nums, int numsSize, int n, int* returnSize){
     int *ans=(int*)malloc(sizeof(int)*numsSize);
     *returnSize=numsSize;
-    for(int i=0,j=i+n,k=0; i<n;i++,j++,k+=2){
-        ans[k]=nums[i];
-        ans[k+1]=nums[j];
+    for(int i=0,j=0; i<n; i++,j+=2){
+        ans[j]=nums[i];
+        ans[j+1]=nums[i+n];
     }
     return ans;
 }
