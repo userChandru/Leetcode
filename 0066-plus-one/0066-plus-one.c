b2649897d24bf39be4fdcd4400081a16dbf6ef5c
Chandru <134100579+userChandru@users.noreply.github.com>
2024-07-19 14:34:11 +0530

    Time: 0 ms (100%), Space: 9.4 MB (5.8%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.c
@@ -0,0 +1,30 @@
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int* ans = (int*)malloc(sizeof(int)*(digitsSize + 1));
+    *returnSize = digitsSize;
+    for(int i=0;i<digitsSize; i++)
+        ans[i+1]=digits[i];
+    ans[0]=0;
+    int sum = ans[digitsSize]+1;
+    int rem = sum%10;
+    sum/=10;
+    if(sum){
+        int n=digitsSize-1;
+        do{
+            if(n==0 && sum==1){
+                ans[0]+=1;
+                *returnSize=digitsSize+1;
+                printf("-%d-\n",*returnSize);
+                break;
+            }
+            ans[n]+=sum;
+            printf("%d",ans[n]);
+            sum=ans[n]/10;
+            ans[n]%=10;
+            printf("=>%d ",ans[n]);
+            n--;
+        }while(sum);
+    }
+    ans[digitsSize]=rem;
+    if(*returnSize==digitsSize) {printf("ii");return (ans+1);}
+    return ans;
+}
