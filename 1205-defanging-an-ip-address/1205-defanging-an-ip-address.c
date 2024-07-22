fede861ee93ae48c9b354c88d04cd79e0ea91d76
Chandru <134100579+userChandru@users.noreply.github.com>
2024-07-22 09:18:19 +0530

    Time: 0 ms (100%), Space: 7.5 MB (21.8%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.c
@@ -0,0 +1,15 @@
+char * defangIPaddr(char * address){
+    int j=0;
+    char* ans = (char*)malloc(strlen(address)+9);
+    for(int i=0; i<strlen(address); i++){
+        if(address[i]=='.'){
+            ans[j++]='[';
+            ans[j++]='.';
+            ans[j++]=']';
+        }
+        else
+            ans[j++]=address[i];
+    }
+    ans[j]='\0';
+    return ans;
+}
