197fd087886d569cf00f98fe40c23b4722876a27
Chandru <134100579+userChandru@users.noreply.github.com>
2024-07-22 15:00:14 +0530

    Time: 0 ms (100%), Space: 7.7 MB (9.09%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.c
@@ -0,0 +1,18 @@
+int getLucky(char* s, int k) {
+    int sum=0,add=0;
+    for(int i=0; s[i]!='\0'; i++){
+        int a=((int)(s[i]-'a')+1);
+        sum+=(a%10)+(a/10);
+        printf("%c-%d sum=%d\n",s[i],a,sum);
+    }
+    k--;
+    while(sum>=10 && k--){
+        while(sum){
+            add+=sum%10;
+            sum/=10;
+        }
+        sum=add;
+        add=0;
+    }
+    return sum;
+}
