3def79cf143bb81fa2db62cb75d1c91981d69934
Chandru <134100579+userChandru@users.noreply.github.com>
2024-07-22 11:15:43 +0530

    Time: 6 ms (84%), Space: 9.5 MB (6%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.c
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.c
@@ -1,20 +1,11 @@
 bool canBeEqual(int* target, int targetSize, int* arr, int arrSize) {
+    int seen[1001]={0};
     for(int i=0; i<targetSize; i++){
-        for(int j=i+1; j<targetSize; j++){
-            if(target[i]>target[j]){
-                int temp=target[i];
-                target[i] = target[j];
-                target[j]=temp;
-            }
-            if(arr[i]>arr[j]){
-                int temp=arr[i];
-                arr[i] = arr[j];
-                arr[j]=temp;
-            }
-        }
+        seen[target[i]]++;
+        seen[arr[i]]--;
     }
-    for(int i=0; i<targetSize; i++)
-        if(target[i]!=arr[i])
+    for(int i=0; i<1001; i++)
+        if(seen[i]!=0)
             return false;
     return true;
 }
