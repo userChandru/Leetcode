ce03dd24f583541edc41afe70b15dbeb6854057d
Chandru <134100579+userChandru@users.noreply.github.com>
2024-07-24 12:18:27 +0530

    Time: 14 ms (36.8%), Space: 10.1 MB (7.2%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.c
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.c
@@ -1,7 +1,10 @@
 int minPartitions(char* n) {
-    int count=0;
-    for(int i=0; i<strlen(n); i++){
-        if((int)(n[i]-'0')>count)count=n[i]-'0';
+    int max=0;
+    max= n[0]-'0';
+    for(int i=1;n[i]!='\0';i++){
+        int temp= n[i]-'0';
+        if(max<temp)
+            max=temp;
     }
-    return count;
+    return max;
 }
