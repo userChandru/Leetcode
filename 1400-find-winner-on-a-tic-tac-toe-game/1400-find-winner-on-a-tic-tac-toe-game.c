5fa84cff854377ae6c714b95d952eb07615c54af
Chandru <134100579+userChandru@users.noreply.github.com>
2024-07-28 01:29:50 +0530

    Time: 2 ms (54.84%), Space: 8.3 MB (6.45%) - LeetHub

---
+++ 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.c
@@ -0,0 +1,34 @@
+char* tictactoe(int** moves, int movesSize, int* movesColSize) {
+    int arr[3][3]={0};
+    
+    for(int i=0; i<movesSize; i++){
+        if(i%2==0)
+            arr[moves[i][0]][moves[i][1]]=1;
+        else
+            arr[moves[i][0]][moves[i][1]]=2;
+    }
+    for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        printf("%d\t",arr[i][j]);
+    }
+    printf("\n");
+}
+    for(int i=0; i<3; i++){
+        if(arr[i][0]==arr[i][1] && arr[i][1]==arr[i][2]){
+            if(arr[i][0]==1) return"A";
+            if(arr[i][0]==2) return"B";
+        }
+        else if(arr[0][i]==arr[1][i] && arr[1][i]==arr[2][i]){
+            if(arr[0][i]==1) return "A";
+            if(arr[0][i]==2) return "B";
+        }
+    }
+    if((arr[0][0]==arr[1][1] && arr[1][1]==arr[2][2])
+    || (arr[0][2]==arr[1][1] && arr[1][1]==arr[2][0]))
+    {
+        if(arr[1][1]==1) return "A";
+        if(arr[1][1]==2) return "B";
+    }
+    if(movesSize==9) return "Draw";
+    return "Pending";
+}
