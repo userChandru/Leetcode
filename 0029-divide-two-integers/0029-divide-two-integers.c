b93bce63eb9c28362376d00f45904c5e23314903
Chandru <134100579+userChandru@users.noreply.github.com>
2024-08-18 14:27:45 +0530

    Time: 0 ms (100%), Space: 8.1 MB (48.14%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.c
+++ 0029-divide-two-integers/0029-divide-two-integers.c
@@ -1,5 +1,6 @@
 int divide(int dividend, int divisor) {
-    if(dividend==INT_MIN && divisor==-1) return INT_MAX;
+    if(dividend==INT_MIN && divisor==-1) 
+        return INT_MAX;
     int res= dividend/divisor;
     return res; 
 }
