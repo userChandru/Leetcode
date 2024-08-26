fafc6be3fc4c664ee1737f0da7ce0da8aa48227a
Chandru <134100579+userChandru@users.noreply.github.com>
2024-08-26 22:42:55 +0530

    Time: 11 ms (69.35%), Space: 12.9 MB (16.13%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.c
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.c
@@ -10,20 +10,20 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-void postTravel(struct Node* root, int* returnSize,int*v){
+void postTravel(struct Node* root, int* returnSize,int*ans){
     if(root == NULL)
         return;
 
     for(int i=0;i<root->numChildren;i++)
-        postTravel(root->children[i], returnSize,v);
+        postTravel(root->children[i], returnSize,ans);
 
-    v[(*returnSize)++]=root->val;
+    ans[(*returnSize)++]=root->val;
 
 }
 
 int* postorder(struct Node* root, int* returnSize) {
     *returnSize=0;
-    int *v=(int*)malloc(sizeof(int)*10000);
-    postTravel(root, returnSize,v);
-    return v;
+    int *ans=(int*)malloc(sizeof(int)*10000);
+    postTravel(root, returnSize,ans);
+    return ans;
 }
