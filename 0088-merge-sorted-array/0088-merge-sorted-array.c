18179db8412845b5b9201dd7f610a0250540fa03
Chandru <134100579+userChandru@users.noreply.github.com>
2024-08-28 00:27:26 +0530

    Time: 3 ms (67.63%), Space: 9.8 MB (64.94%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.c
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -1,7 +1,6 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 
     int i= m-1, j=n-1, k=n+m-1;
-
     while(i >= 0 && j >= 0)
         if(nums1[i] > nums2[j])
             nums1[k--] = nums1[i--];
