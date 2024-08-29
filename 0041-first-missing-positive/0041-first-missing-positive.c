f96029540503359629ae919c73095043dedb033c
Chandru <134100579+userChandru@users.noreply.github.com>
2024-08-29 20:39:43 +0530

    Time: 48 ms (86.3%), Space: 13.8 MB (34.4%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.c
@@ -0,0 +1,15 @@
+int firstMissingPositive(int* nums, int numsSize) {
+    for(int i = 0; i < numsSize; i++)
+        if(nums[i] <= 0)
+            nums[i] = numsSize + 1;
+
+    for(int i = 0; i < numsSize; i++)
+        if((abs(nums[i]) - 1) < numsSize && nums[abs(nums[i])- 1] > 0)
+            nums[abs(nums[i]) - 1] *= -1;
+
+    for(int i = 0; i < numsSize; i++)
+        if(nums[i] > 0) 
+            return i + 1;
+
+    return numsSize+1;
+}
