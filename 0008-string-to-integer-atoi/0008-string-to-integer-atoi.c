4f4d9b916a782c03fdbf6b688f882850e5cfa8d5
Chandru <134100579+userChandru@users.noreply.github.com>
2024-09-01 05:58:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.66%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.c
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.c
@@ -2,13 +2,16 @@
 
 int myAtoi(char* s) {
     int i = 0, sign = 1, num = 0;
-    while (s[i] == ' ') i++;
-    if (s[i] == '-') sign = -1, i++;
-    else if (s[i] == '+') sign = 1, i++;
+    while 
+        (s[i] == ' ') i++;
+    if (s[i] == '-') 
+        sign = -1, i++;
+    else if 
+        (s[i] == '+') sign = 1, i++;
+        
     while (s[i] >= '0' && s[i] <= '9') {
-        if (num > INT_MAX / 10 || (num == INT_MAX / 10 && s[i] - '0' > 7)) {
+        if (num > INT_MAX / 10 || (num == INT_MAX / 10 && s[i] - '0' > 7))
             return sign == 1 ? INT_MAX : INT_MIN;
-        }
         num = num * 10 + (s[i] - '0');
         i++;
     }
