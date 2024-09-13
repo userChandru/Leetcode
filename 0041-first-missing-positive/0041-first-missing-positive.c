37adff3fe6f28e08206ca7088ab40a85107eeedc
Chandru <134100579+userChandru@users.noreply.github.com>
2024-09-13 23:55:51 +0530

    Time: 135 ms (5.05%), Space: 14 MB (32.72%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.c
+++ 0041-first-missing-positive/0041-first-missing-positive.c
@@ -2,10 +2,13 @@ int firstMissingPositive(int* nums, int numsSize) {
     for(int i = 0; i < numsSize; i++)
         if(nums[i] <= 0)
             nums[i] = numsSize + 1;
-
+    
     for(int i = 0; i < numsSize; i++)
-        if((abs(nums[i]) - 1) < numsSize && nums[abs(nums[i])- 1] > 0)
+        if((abs(nums[i]) - 1) < numsSize && nums[abs(nums[i])- 1] > 0){
             nums[abs(nums[i]) - 1] *= -1;
+            printf("%d: %d , ",nums[abs(nums[i]) - 1], nums[i]);
+
+        }
 
     for(int i = 0; i < numsSize; i++)
         if(nums[i] > 0) 
