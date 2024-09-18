0ee2bfdd7fa91514d8446dd5387ee7b0eae321ee
Chandru <134100579+userChandru@users.noreply.github.com>
2024-09-19 01:59:58 +0530

    Time: 366 ms (54.25%), Space: 67.8 MB (98.3%) - LeetHub

--- 3067-modify-columns/3067-modify-columns.py
+++ 3067-modify-columns/3067-modify-columns.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['salary']= 2*employees['salary']
-    return employees
+    return employees.assign(salary=2*employees['salary'])
