63e94307e7d102c39c480cb8c84534944dff3174
Chandru <134100579+userChandru@users.noreply.github.com>
2025-01-28 02:03:08 +0530

    Time: 0 ms (100%), Space: 45.2 MB (35.5%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,50 +1,37 @@
-// import java.util.*;
-
-// Definition for a binary tree node.
-// public class TreeNode {
-//     int val;
-//     TreeNode left;
-//     TreeNode right;
-
-//     TreeNode() {
-//     }
-
-//     TreeNode(int val) {
-//         this.val = val;
-//     }
-
-//     TreeNode(int val, TreeNode left, TreeNode right) {
-//         this.val = val;
-//         this.left = left;
-//         this.right = right;
-//     }
-// }
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
 class Solution {
-    public static void levelorder(TreeNode root, List<List<Integer>> ans) {
+    List<List<Integer>> result;
+
+    public void traverse(TreeNode root, int level) {
         if (root == null)
             return;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                level.add(node.val);
-                if (node.left != null)
-                    queue.offer(node.left);
-                if (node.right != null)
-                    queue.offer(node.right);
-            }
-            ans.add(level);
+        if (level == result.size()) {
+            result.add(new ArrayList<>());
         }
+        result.get(level).add(root.val);
+        traverse(root.left, level + 1);
+        traverse(root.right, level + 1);
     }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        levelorder(root, ans);
-        return ans;
+        result = new ArrayList<>();
+        if (root == null)
+            return result;
+        traverse(root, 0);
+        return result;
     }
 }
