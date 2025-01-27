37190de2887a88e5607e14fc81db04fac9c1277a
Chandru <134100579+userChandru@users.noreply.github.com>
2025-01-27 21:54:05 +0530

    Time: 1 ms (89.9%), Space: 44.9 MB (86.87%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,20 +1,25 @@
-import java.util.*;
+// import java.util.*;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
+// Definition for a binary tree node.
+// public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+
+//     TreeNode() {
+//     }
+
+//     TreeNode(int val) {
+//         this.val = val;
+//     }
+
+//     TreeNode(int val, TreeNode left, TreeNode right) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
 class Solution {
     public static void levelorder(TreeNode root, List<List<Integer>> ans) {
         if (root == null)
@@ -27,7 +32,6 @@ class Solution {
             List<Integer> level = new ArrayList<>();
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
-
                 level.add(node.val);
                 if (node.left != null)
                     queue.offer(node.left);
