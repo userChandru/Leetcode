cfc3bc9ee8fb5c25e07e598283f682ae0f2c12a0
Chandru <134100579+userChandru@users.noreply.github.com>
2025-01-28 10:04:01 +0530

    Time: 0 ms (100%), Space: 42.1 MB (79.33%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,25 +14,21 @@
  * }
  */
 class Solution {
-    List<List<Integer>> levelOrder;
+    List<Integer> ans;
     public void traverse(TreeNode root, int level){
         if(root==null)
             return;
-        if(level == levelOrder.size())
-            levelOrder.add(new ArrayList<>());
-        levelOrder.get(level).add(root.val);
-        traverse(root.left,level+1);
+        if(level == ans.size())
+            ans.add(root.val);
         traverse(root.right,level+1);
+        traverse(root.left,level+1);
     }
     public List<Integer> rightSideView(TreeNode root) {
-        levelOrder = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
+        ans = new ArrayList<>();
         if(root == null)
             return ans;
         traverse(root,0);
-        for(int i=0; i<levelOrder.size(); i++){
-            ans.add(levelOrder.get(i).get(levelOrder.get(i).size()-1));
-        }
+        
         return ans;
     }
 }
