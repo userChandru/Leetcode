9abcd22e9883907187fee7eecf336748bdc92559
Chandru <134100579+userChandru@users.noreply.github.com>
2025-02-03 01:53:27 +0530

    Time: 2 ms (71%), Space: 42.5 MB (49.11%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,54 +1,49 @@
 class Solution {
-
-    int directions[][] = { { 0, -1 }, { 0, 1 }, { 1, 0 }, { -1, 0 } };
-
+    int direction[][] = {{0,1},{0,-1}, {1,0},{-1,0}};
     public int orangesRotting(int[][] grid) {
-        int ROW = grid.length;
-        int COL = grid[0].length;
-        boolean[][] isRotten = new boolean[ROW][COL];
-        boolean willRot = false;
+        int Row = grid.length;
+        int Col = grid[0].length;
+
         Queue<int[]> rottenQueue = new LinkedList<>();
-        for (int i = 0; i < ROW; i++) {
-            for (int j = 0; j < COL; j++) {
-                if (grid[i][j] == 2) {
-                    rottenQueue.add(new int[] { i, j });
+        boolean[][] isRotten = new boolean[Row][Col];
+        boolean willRot = false;
+        for(int i=0; i<Row; i++)
+            for(int j=0; j<Col; j++)
+                if(grid[i][j]==2){
                     isRotten[i][j] = true;
+                    rottenQueue.add(new int[]{i,j});
                 }
-                if(grid[i][j]==1)
-                    willRot = true;
-            }
-        }
-        if(!willRot) return 0;
-        if (rottenQueue.isEmpty())
+                else if(grid[i][j]==1)
+                    willRot=true;
+
+        if(!willRot)
+            return 0;
+        if(rottenQueue.isEmpty())
             return -1;
         int time = -1;
-        while (!rottenQueue.isEmpty()) {
-            int rottenOrange = rottenQueue.size();
-            for (int i = 0; i < rottenOrange; i++) {
-                int[] curr = rottenQueue.poll();
-                for (int[] d : directions) {
-                    int newRow = d[0] + curr[0];
-                    int newCol = d[1] + curr[1];
-                    if (0 > newRow || newRow >= ROW || 0 > newCol || newCol >= COL)
+        while(!rottenQueue.isEmpty()){
+            int size=rottenQueue.size();
+            for(int i=0; i<size; i++){
+                int curr[] = rottenQueue.poll();
+                for(int[] d : direction){
+                    int nR = d[0] + curr[0];
+                    int nC = d[1] + curr[1];
+                    
+                    if(nR < 0 || Row <= nR || nC < 0 || Col <= nC)
                         continue;
-                    if (isRotten[newRow][newCol])
-                        continue;
-                    if (grid[newRow][newCol] == 0)
+                    if(grid[nR][nC] == 0 || isRotten[nR][nC])
                         continue;
 
-                    isRotten[newRow][newCol] = true;
-                    rottenQueue.add(new int[] { newRow, newCol });
+                    isRotten[nR][nC] = true;
+                    rottenQueue.add(new int[]{nR, nC});
                 }
             }
             time++;
         }
-        for (int i = 0; i < ROW; i++) {
-            for (int j = 0; j < COL; j++) {
-                if (grid[i][j] == 1 && isRotten[i][j] == false) {
+        for(int i = 0; i<Row; i++)
+            for(int j=0; j<Col; j++)
+                if(grid[i][j]==1 && !isRotten[i][j])
                     return -1;
-                }
-            }
-        }
         return time;
     }
 }
