af6d9c6ba1c22c4ff4b4442c876f269f808a6c06
Chandru <134100579+userChandru@users.noreply.github.com>
2025-06-10 11:01:15 +0530

    Time: 4 ms (42.51%), Space: 44.5 MB (60.74%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,67 @@
+class Solution {
+    public String intToRoman(int num) {
+        Map<Character, Integer> temp = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000);
+        StringBuilder s = new StringBuilder();
+        
+        while(num>=1000){
+            s.append("M");
+            num-=1000;
+        }
+        if(num>=900){
+            s.append("CM");
+            num-=900;
+        }
+        if(num>=500){
+            s.append("D");
+            num-=500;
+        }
+        if(num>=400){
+            s.append("CD");
+            num-=400;
+        }
+        while(num>=100){
+            s.append("C");
+            num-=100;
+        }
+        if(num>=90){
+            s.append("XC");
+            num-=90;
+        }
+        if(num>=50){
+            s.append("L");
+            num-=50;
+        }
+        if(num>=40){
+            s.append("XL");
+            num-=40;
+        }
+        while(num>=10){
+            s.append("X");
+            num-=10;
+        }
+        if(num>=9){
+            s.append("IX");
+            num-=9;
+        }
+        if(num>=5){
+            s.append("V");
+            num-=5;
+        }
+        if(num>=4){
+            s.append("IV");
+            num-=4;
+        }
+        while(num>=1){
+            s.append("I");
+            num-=1;
+        }
+        return s.toString();
+    }
+}
