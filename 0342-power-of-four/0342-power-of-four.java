0f1d1614c0bf5a139a40200c472f94d30987c9bd
Chandru <134100579+userChandru@users.noreply.github.com>
2025-08-16 15:50:43 +0530

    Time: 0 ms (100%), Space: 41.1 MB (35.21%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,8 +1,9 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        long k=1;
-        while(k<n && k<2147483647)
-            k<<=2;
-        return k==n;
+        // long k=1;
+        // while(k<n && k<2147483647)
+        //     k<<=2;
+        // return k==n;
+        return n>0 && ((n & (n-1)) == 0) && ((n & 0x55555555) != 0);
    }
 }
