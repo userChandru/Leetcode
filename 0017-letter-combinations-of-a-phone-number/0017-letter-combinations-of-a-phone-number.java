cbd3e7ae6fd5597e6c3d8ef2a85e7127a4ea5d1c
Chandru <134100579+userChandru@users.noreply.github.com>
2025-08-27 02:42:58 +0530

    Time: 0 ms (100%), Space: 42.1 MB (78.33%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,25 +1,26 @@
 class Solution {
-
-    List<String> ans;
-    StringBuilder temp;
-    static Map<Character, Character[]> map = new HashMap<>();
+    private static final Map<Character, char[]> map = new HashMap<>();
     static {
-        map.put('2', new Character[] { 'a', 'b', 'c' });
-        map.put('3', new Character[] { 'd', 'e', 'f' });
-        map.put('4', new Character[] { 'g', 'h', 'i' });
-        map.put('5', new Character[] { 'j', 'k', 'l' });
-        map.put('6', new Character[] { 'm', 'n', 'o' });
-        map.put('7', new Character[] { 'p', 'q', 'r', 's' });
-        map.put('8', new Character[] { 't', 'u', 'v' });
-        map.put('9', new Character[] { 'w', 'x', 'y', 'z' });
+        map.put('2', new char[] { 'a', 'b', 'c' });
+        map.put('3', new char[] { 'd', 'e', 'f' });
+        map.put('4', new char[] { 'g', 'h', 'i' });
+        map.put('5', new char[] { 'j', 'k', 'l' });
+        map.put('6', new char[] { 'm', 'n', 'o' });
+        map.put('7', new char[] { 'p', 'q', 'r', 's' });
+        map.put('8', new char[] { 't', 'u', 'v' });
+        map.put('9', new char[] { 'w', 'x', 'y', 'z' });
     }
 
-    public void helper(String digits, int i) {
-        if (temp.length() == digits.length()){
+    private List<String> ans;
+    private StringBuilder temp;
+
+    private void helper(String digits, int i) {
+        if (temp.length() == digits.length()) {
             ans.add(temp.toString());
             return;
-        }       
-        for (Character c : map.get(digits.charAt(i))) {
+        }
+        char[] letters = map.get(digits.charAt(i));
+        for (char c : letters) {
             temp.append(c);
             helper(digits, i + 1);
             temp.deleteCharAt(temp.length() - 1);
@@ -27,20 +28,12 @@ class Solution {
     }
 
     public List<String> letterCombinations(String digits) {
-        ans = new LinkedList<>();
+        ans = new ArrayList<>();
         temp = new StringBuilder();
-        if(digits.length()==0)
-            return ans;
 
-        map.put('2', new Character[] { 'a', 'b', 'c' });
-        map.put('3', new Character[] { 'd', 'e', 'f' });
-        map.put('4', new Character[] { 'g', 'h', 'i' });
-        map.put('5', new Character[] { 'j', 'k', 'l' });
-        map.put('6', new Character[] { 'm', 'n', 'o' });
-        map.put('7', new Character[] { 'p', 'q', 'r', 's' });
-        map.put('8', new Character[] { 't', 'u', 'v' });
-        map.put('9', new Character[] { 'w', 'x', 'y', 'z' });
-        this.helper(digits, 0);
+        if (digits.isEmpty()) return ans;
+
+        helper(digits, 0);
         return ans;
     }
 }
