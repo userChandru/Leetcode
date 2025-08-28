bcffb59f43d78f584e3196c02db20a5fbc2dff8e
Chandru <134100579+userChandru@users.noreply.github.com>
2025-08-29 00:36:52 +0530

    Time: 2 ms (100%), Space: 45.6 MB (57.8%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.java
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.java
@@ -2,15 +2,16 @@ class Solution {
     public int[][] sortMatrix(int[][] grid) {
         int r = grid.length, c = grid[0].length;
         int i = r, j = 0;
+        int m, n, t;
         while (i != 0 || j != c - 1) {
             if (i > 0) {
                 i--;
-                int m = 0;
+                m = 0;
                 while (m + i < r) {
-                    int n = m + 1;
+                    n = m + 1;
                     while (n + i < r) {
                         if (grid[i + m][j + m] < grid[i + n][j + n]) {
-                            int t = grid[i + m][j + m];
+                            t = grid[i + m][j + m];
                             grid[i + m][j + m] = grid[i + n][j + n];
                             grid[i + n][j + n] = t;
                         }
@@ -22,12 +23,12 @@ class Solution {
             }
             if (j < c - 1) {
                 j++;
-                int m = 0;
+                m = 0;
                 while (m + j < c) {
-                    int n = m + 1;
+                    n = m + 1;
                     while (n + j < c) {
                         if (grid[i + m][j + m] > grid[i + n][j + n]) {
-                            int t = grid[i + m][j + m];
+                            t = grid[i + m][j + m];
                             grid[i + m][j + m] = grid[i + n][j + n];
                             grid[i + n][j + n] = t;
                         }
