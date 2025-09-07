d065d3e5536d69c29fa7cde63edaf0d62ce6e5e4
Chandru <134100579+userChandru@users.noreply.github.com>
2025-09-07 11:30:20 +0530

    Time: 21 ms (92.05%), Space: 46.1 MB (32.89%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,8 +1,13 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
         HashSet<Integer> hs = new HashSet<>();
+        int i=0, n=candyType.length / 2;
         for (int c : candyType)
-            hs.add(c);
-        return Math.min(hs.size(), candyType.length / 2);
+            if(hs.add(c)){
+                i++;
+                if(i>=n)
+                    return n;
+            }         
+        return hs.size();
     }
 }
