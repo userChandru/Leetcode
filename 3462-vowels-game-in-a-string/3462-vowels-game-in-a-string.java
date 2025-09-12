f5f6cc19ebae670f107955ab6ec54297c0b7d88d
Chandru <134100579+userChandru@users.noreply.github.com>
2025-09-12 19:27:41 +0530

    Time: 5 ms (67.45%), Space: 45.4 MB (82.68%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean doesAliceWin(String s) {
 
-        // HashSet<Character> vow= new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
         for(char ch: s.toCharArray())
-            switch(ch){
+            switch (ch) {
                 case 'a','e','i','o','u','A','E','I','O','U':
                     return true; 
             }
