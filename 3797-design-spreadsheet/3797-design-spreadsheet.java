d0978a93d4834613cf336e8e9fc01236fd0b838f
Chandru <134100579+userChandru@users.noreply.github.com>
2025-09-19 21:02:07 +0530

    Time: 131 ms (29.93%), Space: 56.2 MB (40.14%) - LeetHub

---
+++ 3797-design-spreadsheet/3797-design-spreadsheet.java
@@ -0,0 +1,43 @@
+class Spreadsheet {
+    HashMap<Character, int[]> sheet;
+    public Spreadsheet(int rows) {
+        sheet = new HashMap<>();
+        for(char ch = 'A'; ch<='Z'; ch++)
+            sheet.put(ch, new int[rows]);
+    }
+    
+    public void setCell(String cell, int value) {
+        char col = cell.charAt(0);
+        int row = Integer.parseInt(cell.substring(1))-1;
+        sheet.get(col)[row]=value;
+    }
+    
+    public void resetCell(String cell) {
+        char col = cell.charAt(0);
+        int row = Integer.parseInt(cell.substring(1))-1;
+        sheet.get(col)[row]=0;
+    }
+    
+    public int getValue(String formula) {
+        int sum=0;
+        String cell1= (formula.split("\\+")[0]).substring(1);
+        String cell2= (formula.split("\\+")[1]);
+        if(Character.isDigit(cell1.charAt(0)))
+            sum+=Integer.parseInt(cell1);
+        else 
+            sum+=sheet.get(cell1.charAt(0))[Integer.parseInt(cell1.substring(1))-1];
+        if(Character.isDigit(cell2.charAt(0)))
+            sum+=Integer.parseInt(cell2);
+        else 
+            sum+=sheet.get(cell2.charAt(0))[Integer.parseInt(cell2.substring(1))-1];
+        return sum;
+    }
+}
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet obj = new Spreadsheet(rows);
+ * obj.setCell(cell,value);
+ * obj.resetCell(cell);
+ * int param_3 = obj.getValue(formula);
+ */
