b99841750a5dfa151ad521d7b436f3353c8ee103
Chandru <134100579+userChandru@users.noreply.github.com>
2025-10-03 23:28:15 +0530

    Time: 19 ms (77.46%), Space: 46.3 MB (71.37%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -1,39 +1,40 @@
 class Solution {
-    public int trapRainWater(int[][] heightMap) {
-        int m = heightMap.length, n = heightMap[0].length;
+    public int trapRainWater(int[][] map) {
+        int m = map.length, n = map[0].length;
         
-        if (m < 3 || n < 3)
-            return 0;
-
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        boolean[][] visited = new boolean[m][n];
-        int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        boolean[][] seen = new boolean[m][n];
+        int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
 
         for (int i = 0; i < m; i++) {
-            pq.offer(new int[] { heightMap[i][0], i, 0 });
-            pq.offer(new int[] { heightMap[i][n - 1], i, n - 1 });
-            visited[i][0] = true;
-            visited[i][n - 1] = true;
+            pq.offer(new int[] { map[i][0], i, 0 });
+            pq.offer(new int[] { map[i][n - 1], i, n - 1 });
+            seen[i][0] = true;
+            seen[i][n - 1] = true;
         }
         for (int j = 0; j < n; j++) {
-            pq.offer(new int[] { heightMap[0][j], 0, j });
-            pq.offer(new int[] { heightMap[m - 1][j], m - 1, j });
-            visited[0][j] = true;
-            visited[m - 1][j] = true;
+            pq.offer(new int[] { map[0][j], 0, j });
+            pq.offer(new int[] { map[m - 1][j], m - 1, j });
+            seen[0][j] = true;
+            seen[m - 1][j] = true;
         }
 
         int result = 0;
-        
+
         while (!pq.isEmpty()) {
             int[] cell = pq.poll();
-            int height = cell[0], x = cell[1], y = cell[2];
+            int h = cell[0];
+            int x = cell[1];
+            int y = cell[2];
 
-            for (int[] dir : directions) {
+            for (int[] dir : dirs) {
                 int nx = x + dir[0], ny = y + dir[1];
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[nx][ny]) {
-                    result += Math.max(0, height - heightMap[nx][ny]);
-                    pq.offer(new int[] { Math.max(height, heightMap[nx][ny]), nx, ny });
-                    visited[nx][ny] = true;
+
+                if (nx >= 0 && ny >= 0 && nx < m && ny < n && !seen[nx][ny]) {
+
+                    result += Math.max(0, h - map[nx][ny]);                    
+                    pq.offer(new int[] { Math.max(h, map[nx][ny]), nx, ny });
+                    seen[nx][ny] = true;
                 }
             }
         }
