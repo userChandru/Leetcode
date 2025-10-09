30b4942819b1d21ef8f0ee7475f8116cfca20915
Chandru <134100579+userChandru@users.noreply.github.com>
2025-10-10 00:47:34 +0530

    Time: 93 ms (90.14%), Space: 45.1 MB (28.17%) - LeetHub

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.java
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.java
@@ -3,20 +3,21 @@ class Solution {
         int n = skill.length;
         int m = mana.length;
 
-        long[] arr = new long[n];
-        
-        for (int i = 0; i < m; i++) {
-            long curr = mana[i], min = arr[0]; 
-            
-            for (int j = 1; j < n; j++) 
-                min = Math.max(min + (long) skill[j - 1] * curr, arr[j]);
-            
-            arr[n - 1] = min + (long) skill[n - 1] * curr;
-            
-            for (int j = n - 2; j >= 0; j--)
-                arr[j] = arr[j + 1] - (long) skill[j + 1] * curr;
+        long[] prev = new long[n];
+        for (int i = 1; i < n; i++) 
+            prev[i] = prev[i - 1] + skill[i];
+
+        long sum = (long) skill[0] * mana[0];
+
+        for (int j = 1; j < m; j++) {
+            long max = (long) skill[0] * mana[j];
+            for (int i = 1; i < n; i++) {
+                long diff = prev[i] * mana[j - 1] - prev[i - 1] * mana[j];
+                if (diff > max) 
+                    max = diff;
+            }
+            sum += max;
         }
-        
-        return arr[n - 1];
+        return sum + prev[n - 1] * mana[m - 1];
     }
 }
