4b8b9b274885867240e09ea5ad692d0c872bfd46
Chandru <134100579+userChandru@users.noreply.github.com>
2025-10-12 22:13:46 +0530

    Time: 38 ms (95.12%), Space: 52.7 MB (68.29%) - LeetHub

--- 3851-find-sum-of-array-product-of-magical-sequences/3851-find-sum-of-array-product-of-magical-sequences.java
+++ 3851-find-sum-of-array-product-of-magical-sequences/3851-find-sum-of-array-product-of-magical-sequences.java
@@ -1,61 +1,74 @@
-import java.util.*;
-
 class Solution {
-    int MOD = 1000000007, MAX = 31;
-    long[] fact = new long[MAX], invFact = new long[MAX];
+    int MOD = 1_000_000_007;
 
-    public long modPow(long a, int b) {
-        long res = 1;
-        while (b > 0) {
-            if ((b & 1) == 1) res = res * a % MOD;
-            a = a * a % MOD;
-            b >>= 1;
+    int[][] comb;
+    Integer[][][][] dp;
+    int[] nums;
+    int n;
+
+    public int magicalSum(int m, int k, int[] numbers) {
+        n = numbers.length;
+        nums = numbers;
+        comb = comb(m, m);
+        dp = new Integer[m + 1][k + 1][n][m + 1];
+        return dp(m, k, 0, 0);
+    }
+
+    private int dp(int m, int k, int i, int carry) {
+        if (m < 0 || k < 0) return 0;
+        if (i == n) 
+            return (m == 0 && k - Integer.bitCount(carry) == 0) ? 1 : 0;
+
+        if(dp[m][k][i][carry] != null) 
+            return dp[m][k][i][carry];
+        
+        int res = 0;
+        for(int count = 0; count <= m; count++) {
+            int newM = m - count;
+            int temp = carry + count;
+            int newK = k - temp % 2;
+            int newCarry = temp / 2;
+            int sub = dp(newM, newK, i + 1, newCarry);
+            if(sub == 0) 
+                continue;
+            int adding = mult(comb[m][count], modPow(nums[i], count));
+            res = add(res, mult(sub, adding));
+        }
+        return dp[m][k][i][carry] = res;
+    }
+
+    private int[][] comb(int a, int b) {
+        int[][] comb = new int[a + 1][b + 1];
+        comb[0][0] = 1;
+        for(int i = 1; i <= a; i++) {
+            for(int j = 0; j <= b; j++) {
+                if(j == 0) {
+                    comb[i][0] = 1;
+                    continue;
+                }
+                comb[i][j] = add(comb[i - 1][j - 1], comb[i - 1][j]);
+            }
+        }
+        return comb;
+    }
+
+    private int modPow(int a, int b) {
+        int res = 1;
+        while(b > 0) {
+            if((b & 1) == 1) {
+                res = mult(res, a);
+            }
+            a = mult(a, a);
+            b /= 2;
         }
         return res;
     }
 
-    public int magicalSum(int m, int k, int[] nums) {
-
-        fact[0] = 1;
-        for (int i = 1; i < MAX; i++) 
-            fact[i] = fact[i - 1] * i % MOD;
-
-        invFact[MAX - 1] = modPow(fact[MAX - 1], MOD - 2);
-        
-        for (int i = MAX - 1; i > 0; i--) 
-            invFact[i - 1] = invFact[i] * i % MOD;
-
-        int n = nums.length;
-        long[][] pw = new long[n][m + 1];
-        for (int i = 0; i < n; i++) {
-            pw[i][0] = 1;
-            for (int j = 1; j <= m; j++) pw[i][j] = pw[i][j - 1] * nums[i] % MOD;
-        }
-
-        long[][][][] dp = new long[n + 1][m + 1][m / 2 + 2][k + 1];
-        for (long[][][] a : dp)
-            for (long[][] b : a)
-                for (long[] c : b)
-                    Arrays.fill(c, -1);
-
-        long res = dfs(0, m, 0, k, pw, dp);
-        return (int) (res * fact[m] % MOD);
+    public int mult(int a, int b) {
+        return (int) (((long) a * b) % MOD);
     }
 
-    public long dfs(int idx, int left, int carry, int bits, long[][] pw, long[][][][] dp) {
-        int ones = Integer.bitCount(carry);
-        if (ones + left < bits) return 0;
-        if (idx == pw.length) return (left == 0 && ones == bits) ? 1 : 0;
-        if (dp[idx][left][carry][bits] != -1) return dp[idx][left][carry][bits];
-
-        long ans = 0;
-        for (int j = 0; j <= left; j++) {
-            int bit = (carry + j) & 1;
-            if (bit <= bits) {
-                long next = dfs(idx + 1, left - j, (carry + j) >> 1, bits - bit, pw, dp);
-                ans = (ans + next * pw[idx][j] % MOD * invFact[j]) % MOD;
-            }
-        }
-        return dp[idx][left][carry][bits] = ans;
+    public int add(int a, int b) {
+        return (int) (((long) a + b) % MOD);
     }
 }
