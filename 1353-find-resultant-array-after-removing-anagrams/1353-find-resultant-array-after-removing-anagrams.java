72d88e2b484138efc7d80458e5fb813e689d6e9e
Chandru <134100579+userChandru@users.noreply.github.com>
2025-10-13 23:27:08 +0530

    Time: 1 ms (99.78%), Space: 44.6 MB (92.8%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
@@ -1,20 +1,28 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        int n = words.length;
-        List<Map<Character, Integer>> freq = new ArrayList<>();
-        for (String w : words) {
-            Map<Character, Integer> map = new HashMap<>();
-            for (char c : w.toCharArray())
-                map.put(c, map.getOrDefault(c, 0) + 1);
-            freq.add(map);
-        }
-
-        List<String> ans = new ArrayList<>();
-        ans.add(words[0]);
-        for (int i = 1; i < n; i++) {
-            if (!freq.get(i).equals(freq.get(i - 1)))
-                ans.add(words[i]);
-        }
-        return ans;
+        List<String> list= new ArrayList<>();
+        list.add(words[0]);
+        for(int i=1;i<words.length;i++)
+                if(!isAnagram(words[i],words[i-1]))
+                    list.add(words[i]);
+                    
+        return list;   
     }
+    public boolean isAnagram(String s, String t){
+        if(s.length()!=t.length())
+            return false;
+
+        int[]count= new int[26];
+
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int c:count)
+            if(c!=0)
+                return false;
+
+        return true;
+    }
+   
 }
