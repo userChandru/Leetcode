97d70c4b19713fbda79d27673b71effb9497598d
Chandru <134100579+userChandru@users.noreply.github.com>
2025-10-31 20:53:02 +0530

    Time: 1 ms (100%), Space: 45.7 MB (7.71%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -11,29 +11,10 @@ class Solution {
                 ans[idx++]=num;
             if(idx==2)
                 return ans;
-            nums[num] *= -1;
-            if(nums[num]==0)
+            int t = nums[num] *= -1;
+            if(t==0)
                 nums[num]=-10000;
         }
         return ans;
     }
 }
-
-
-// class Solution {
-//     public int[] getSneakyNumbers(int[] nums) {
-//         int n=nums.length-2, i=0;
-//         int ans[]= new int[2];
-//         int freq[] = new int[n];
-//         Arrays.fill(freq,0);
-//         for(int num: nums){
-//             if(i==2)
-//                 return ans;
-//             if(freq[num]++ == 1)
-//                 ans[i++] = num;
-//         }
-//         return ans;
-//     }
-// }
-
-
