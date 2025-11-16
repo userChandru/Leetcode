be671962424346635bc235ca4143fce2da68b1e1
Chandru <134100579+userChandru@users.noreply.github.com>
2025-11-16 17:05:03 +0530

    Time: 4 ms (90.05%), Space: 46.2 MB (14.66%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
@@ -1,16 +1,12 @@
-class Solution {
-    public int numSub(String s) {
-        long ans = 0;
-        long count = 0;
-        char[] arr = s.toCharArray();
-        for (char c : arr)
-            if (c == '1') {
-                count++;
-                ans = (ans + count) % 1000000007;
-            } 
-            else
-                count = 0;
-
-        return (int)ans;
+class Solution{
+    public static int numSub(String s) {
+        int ones = 0 ;
+        int ans = 0;
+        for(char ch : s.toCharArray())
+            if (ch  == '1')
+                ans = (ans + ++ones) % 1000000007 ;
+            else    
+                ones = 0 ;
+        return ans;
     }
 }
