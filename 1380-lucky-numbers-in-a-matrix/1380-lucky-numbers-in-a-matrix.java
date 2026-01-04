00d1dba2915f002f6190a53cc114ed12a1d4c15c
Chandru <134100579+userChandru@users.noreply.github.com>
2026-01-04 16:51:40 +0530

    Time: 3 ms (46.94%), Space: 47.4 MB (15.83%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
@@ -1,29 +1,26 @@
 class Solution {
     public List<Integer> luckyNumbers(int[][] matrix) {
-        List<Integer> ans = new LinkedList<>();
         int m = matrix.length;
         int n = matrix[0].length;
-        int row[] = new int[m];
-        int col[] = new int[n];
-        Arrays.fill(row, 100001);
-        
-        for(int i = 0; i<m; i++)
-            for(int j=0; j<n; j++)
-                row[i] = Math.min(row[i], matrix[i][j]);
+        int rowMin[] = new int[m];
+        int colMax[] = new int[n];
 
-        for(int j=0; j<n; j++)
-            for(int i = 0; i<m; i++)
-                col[j] = Math.max(col[j], matrix[i][j]);
-        Set<Integer> set = new HashSet<>();
-        
-        for(int num: row)
-            if(!set.add(num))
-                ans.add(num);
-        
-        for(int num: col)
-            if(!set.add(num))
-                ans.add(num);
-        
+        Arrays.fill(rowMin, Integer.MAX_VALUE);
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                rowMin[i] = Math.min(rowMin[i], matrix[i][j]);
+
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < m; i++) 
+                colMax[j] = Math.max(colMax[j], matrix[i][j]);
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j] == rowMin[i] && matrix[i][j] == colMax[j]) 
+                    ans.add(matrix[i][j]);
         return ans;
     }
 }
