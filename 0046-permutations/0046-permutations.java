85cf5e566f030d13f0f7520489cf6e2b5472aa3f
Chandru <134100579+userChandru@users.noreply.github.com>
2026-01-18 02:53:35 +0530

    Time: 0 ms (100%), Space: 45.3 MB (83.32%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,30 @@
 class Solution {
-    List<List<Integer>> list;
 
-    public void helper(List<Integer> currList, int[] nums) {
-        if (currList.size() == nums.length)
-            list.add(new ArrayList<>(currList));
-        else 
-            for (int i = 0; i < nums.length; i++) {
-                if (currList.contains(nums[i]))
-                    continue;
-                currList.add(nums[i]);
-                helper(currList, nums);
-                currList.remove(currList.size() - 1);
-            }
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
+
+    public void helper(int[] nums, int i) {
+        if (i == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int num : nums)
+                list.add(num);
+            ans.add(list);
+            return;
+        }
+        for (int j = i; j < nums.length; j++) {
+            swap(nums, i, j);
+            helper(nums, i + 1);
+            swap(nums, i, j);
+        }
+    }
+
     public List<List<Integer>> permute(int[] nums) {
-        list = new ArrayList<>();
-        helper(new ArrayList<>(), nums);
-        return list;
+        helper(nums, 0);
+        return ans;
     }
-
 }
