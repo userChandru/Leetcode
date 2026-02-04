acce1e4785820990229a1873082b9a12027730f9
Chandru <134100579+userChandru@users.noreply.github.com>
2026-02-05 01:40:29 +0530

    Time: 0 ms (100%), Space: 42.2 MB (70.83%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int ans = 0;
+        while (x != y) {
+            if ((x & 1) != (y & 1))
+                ans++;
+            x >>= 1;
+            y >>= 1;
+        }
+        return ans;
+    }
+}
